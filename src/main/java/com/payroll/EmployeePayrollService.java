9e04fee475037b7d8bacc24b818701eebb49b667
Deeksha <smdeeksha2005@gmail.com>
2026-03-06 17:14:24 +0530

    [Deeksha]: UC2: Retrieve employee payroll data

--- src/main/java/com/payroll/EmployeePayrollService.java
+++ src/main/java/com/payroll/EmployeePayrollService.java
@@ -3,6 +3,8 @@ package com.payroll;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class EmployeePayrollService {
 
@@ -17,5 +19,26 @@ public class EmployeePayrollService {
         System.out.println("Database Connected Successfully");
 
         return connection;
+    }
+
+    public void readEmployeeData() {
+
+        String query = "SELECT * FROM employee_payroll";
+
+        try (Connection connection = this.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+
+                System.out.println(id + " | " + name + " | " + salary);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/payroll/Main.java
+++ src/main/java/com/payroll/Main.java
@@ -1,26 +1,12 @@
 package com.payroll;
 
-import java.sql.Connection;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
+        EmployeePayrollService service = new EmployeePayrollService();
 
-            EmployeePayrollService service = new EmployeePayrollService();
-
-            Connection connection = service.getConnection();
-
-            if (connection != null) {
-                System.out.println("Connection Test Successful");
-            } else {
-                System.out.println("Connection Test Failed");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        service.readEmployeeData();
 
     }
 }
